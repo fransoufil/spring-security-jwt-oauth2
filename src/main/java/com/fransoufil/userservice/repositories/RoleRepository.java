da61ec86fc0fb258126019b2668c9e65b9ab5627
fransoufil <fran.sf@uol.com.br>
2022-05-22 08:06:20 -0300

    Project created; domains User and Role; Repositories for both

---
+++ src/main/java/com/fransoufil/userservice/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.fransoufil.userservice.repositories;
+
+import com.fransoufil.userservice.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long > {
+    Role findByName(String name);
+}
---
+++ src/main/java/com/fransoufil/userservice/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.fransoufil.userservice.repositories;
+
+import com.fransoufil.userservice.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
